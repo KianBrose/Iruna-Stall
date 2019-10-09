ab869d32992081ecd35f622f37f200d801aa1c91
Abhishek Kumar <akumar@hotmail.co.in>
2019-10-09 11:23:01 +0530

    include partial default layout of the front page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,61 +1,114 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Iruna Store</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <!-- bootstrap css -->
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-    </head>
-    <body>
+    <title>Iruna Store</title>
 
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <a class="navbar-brand" href="#">Iruna Store</a>
+    <!-- bootstrap css -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+</head>
 
-            <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
-                <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+<body>
 
-                   @if (Route::has('login'))
-                        @auth
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <a class="navbar-brand" href="#">Iruna Store</a>
+
+        <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
+            <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+
+                @if (Route::has('login')) 
+                    @auth
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ url('/home') }}">Home</a>
+                        </li>
+                    @else
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('login') }}">Home</a>
+                        </li>
+                        @if (Route::has('register'))
                             <li class="nav-item">
-                                <a class="nav-link" href="{{ url('/home') }}">Home</a>
+                                <a class="nav-link" href="{{ route('register') }}">Home</a>
                             </li>
-                        @else
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">Home</a>
-                            </li>
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">Home</a>
-                                </li>
-                            @endif
-                        @endauth
-                     @endif
+                        @endif 
+                    @endauth 
+                @endif
 
+            </ul>
+            <form class="form-inline my-2 my-lg-0">
+                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+            </form>
+        </div>
+    </nav>
+    <div class="container">
+        <div class="row" style="height: auto !important;">
+            <div class="col-md-7 offset-md-3" style="height: auto !important; min-height: 0px !important;">
 
-                </ul>
-                <form class="form-inline my-2 my-lg-0">
-                    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-                    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-                </form>
+                <div style="height: auto !important;">
+                    <h4>Welcome!</h4>
+                    <hr>
+
+                    <form class="form-horizontal" name="form1" id="form1" action="/search" method="get">
+                        <div class="row">
+                            <div class="col-md-9 col-xs-8">
+                                <input type="text" class="form-control" name="search" id="search" value="">
+                            </div>
+                            <div class="col-md-2 col-xs-3">
+                                <input type="button" class="btn btn-default btn-block" name="search_button" id="search_button" value="Search">
+                            </div>
+                        </div>
+                    </form>
+                    <div class="row divider" role="separator" style="margin-top:20px;"></div>
+
+                    <p>Welcome to our Iruna Database</p>
+                    <br> Uncheck to Reduce Data
+                    <br>
+                    <input type="checkbox" id="mob_images"> Display Monsters
+                    <br>
+                    <input type="checkbox" id="item_images" > Display Items
+                    <br>
+                    <br>
+                    <br>
+
+                    <div class="row divider" role="separator" style="margin-top:20px;"></div>
+                </div>
+                <div></div>
             </div>
-        </nav>
-            <div class="container">
-                <div class="title m-b-md">
-                    Iruna Store
+            <div class="col-md-2">
+                <div>
+
+                    <h4>&nbsp;</h4>
+                    <hr>
+
+
+                    <div>
+                        <h4>Useful Links</h4></div>
+                    <ul class="decoration_none">
+                        <li><a href="http://iruna-online.com/">Official Iruna Website</a></li>
+                        <li><a href="http://irunaonline.boards.net/">Iruna Boards Forum</a></li>
+                        <li><a href="http://iruna-online.weebly.com/">Iruna Weebly</a></li>
+                        <li><a href="https://wikiwiki.jp/iruna-online/">Iruna JP Wiki</a></li>
+                        <li><a href="https://ameblo.jp/irunaken217/">Ameblo JP - Irunaken217</a></li>
+                    </ul>
+                    <br>
+
                 </div>
             </div>
+        </div>
+    </div>
 
-        <!-- bootstrap js -->
-        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-    </body>
+    <!-- bootstrap js -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</body>
+
 </html>
