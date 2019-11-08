7990d8a33bb94ed14821690b0bc00862add18a86
17kibr <17kibr@stockholmscience.se>
2019-11-08 23:41:23 +0100

    width test

--- resources/views/viewitem.blade.php
+++ resources/views/viewitem.blade.php
@@ -18,43 +18,43 @@
 						<thead>
 							<tr class="table100-head">
 								<th class="column1">Name</th>
-								<th class="column1">ID</th>
-								<th class="column1">ATK</th>
-								<th class="column1">DEF</th>
-								<th class="column1">Refinement</th>
-								<th class="column1">Slots</th>
-								<th class="column1">Slot 1</th>
-								<th class="column1">Slot 2</th>
-								<th class="column1">Ability</th>
-								<th class="column1">Ability Lv</th>
+								<th class="column4">ID</th>
+								<th class="column4">ATK</th>
+								<th class="column4">DEF</th>
+								<th class="column4">Refinement</th>
+								<th class="column4">Slots</th>
+								<th class="column4">Slot 1</th>
+								<th class="column4">Slot 2</th>
+								<th class="column4">Ability</th>
+								<th class="column4">Ability Lv</th>
 								<th class="column1">Price</th>
 							</tr>
 						</thead>
 						<tbody>
 							<tr>
 								<td class="column1">1</td>
-								<td class="column1">2</td>
-								<td class="column1">3</td>
-								<td class="column1">4</td>
-								<td class="column1">5</td>
-								<td class="column1">6</td>
-								<td class="column1">7</td>
-								<td class="column1">8</td>
-								<td class="column1">9</td>
-								<td class="column1">10</td>
+								<td class="column4">2</td>
+								<td class="column4">3</td>
+								<td class="column4">4</td>
+								<td class="column4">5</td>
+								<td class="column4">6</td>
+								<td class="column4">7</td>
+								<td class="column4">8</td>
+								<td class="column4">9</td>
+								<td class="column4">10</td>
 								<td class="column1">11</td>
 							</tr>
 							<tr>
 								<td class="column1">1</td>
-								<td class="column1">2</td>
-								<td class="column1">3</td>
-								<td class="column1">4</td>
-								<td class="column1">5</td>
-								<td class="column1">6</td>
-								<td class="column1">7</td>
-								<td class="column1">8</td>
-								<td class="column1">9</td>
-								<td class="column1">10</td>
+								<td class="column4">2</td>
+								<td class="column4">3</td>
+								<td class="column4">4</td>
+								<td class="column4">5</td>
+								<td class="column4">6</td>
+								<td class="column4">7</td>
+								<td class="column4">8</td>
+								<td class="column4">9</td>
+								<td class="column4">10</td>
 								<td class="column1">11</td>
 							</tr>
 						</tbody>
