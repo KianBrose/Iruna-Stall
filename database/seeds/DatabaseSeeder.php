c561a434b0a095098d8f2c0d19266338cf839256
polowis <tranphuochung0912@gmail.com>
2019-11-09 13:33:59 +1100

    Update DatabaseSeeder.php

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,18 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        Eloquent::unguard();
+
+        $path = 'database/irunaitem/item1.sql';
+        DB::unprepared(file_get_contents($path));
+        $this->command->info('Migrated iruna database 1/3');
         // $this->call(UsersTableSeeder::class);
+        $path = 'database/irunaitem/item2.sql';
+        DB::unprepared(file_get_contents($path));
+        $this->command->info('Migrated iruna database 2/3');
+
+        $path = 'database/irunaitem/item3.sql';
+        DB::unprepared(file_get_contents($path));
+        $this->command->info('Migrated iruna database 3/3');
     }
 }
