29ab30d9557a4b9cfefd250b6f83151ee3bff847
polowis <tranphuochung0912@gmail.com>
2019-11-09 13:45:12 +1100

    Dump sql file

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -15,14 +15,18 @@ class DatabaseSeeder extends Seeder
 
         $path = 'database/irunaitem/item1.sql';
         DB::unprepared(file_get_contents($path));
-        $this->command->info('Migrated iruna database 1/3');
+        $this->command->info('Migrated iruna database 1/4');
         // $this->call(UsersTableSeeder::class);
         $path = 'database/irunaitem/item2.sql';
         DB::unprepared(file_get_contents($path));
-        $this->command->info('Migrated iruna database 2/3');
+        $this->command->info('Migrated iruna database 2/4');
 
         $path = 'database/irunaitem/item3.sql';
         DB::unprepared(file_get_contents($path));
-        $this->command->info('Migrated iruna database 3/3');
+        $this->command->info('Migrated iruna database 3/4');
+
+        $path = 'database/irunaitem/item4.sql';
+        DB::unprepared(file_get_contents($path));
+        $this->command->info('Migrated iruna database 4/4');
     }
 }
