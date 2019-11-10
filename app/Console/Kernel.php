5657ed37374da1c1125a65241e02ed65019f2ac5
polowis <tranphuochung0912@gmail.com>
2019-11-11 09:28:29 +1100

    add function to delete item after 30 days

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,6 +4,7 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use Illuminate\Support\Facades\DB;
 
 class Kernel extends ConsoleKernel
 {
@@ -26,6 +27,13 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')
         //          ->hourly();
+        $schedule->call(function(){
+            DB::table('ai')->where('created_at', '<', 'NOW() - INTERVAL 30 DAYS')->delete()->hourly();
+            DB::table('relic')->where('created_at', '<', 'NOW() - INTERVAL 30 DAYS')->delete();
+            DB::table('xtal')->where('created_at', '<', 'NOW() - INTERVAL 30 DAYS')->delete();
+            DB::table('equipment')->where('created_at', '<', 'NOW() - INTERVAL 30 DAYS')->delete();
+            DB::table('item')->where('created_at', '<', 'NOW() - INTERVAL 30 DAYS')->delete();
+        })->hourly();
     }
 
     /**
