8ceadb9143e10d056614c072fc1328b017aacc7b
Kian Bröse Brito <45287789+17kibr@users.noreply.github.com>
2019-11-13 10:48:07 +0100

    Update viewitem.blade.php

--- resources/views/viewitem.blade.php
+++ resources/views/viewitem.blade.php
@@ -35,22 +35,42 @@
 						</thead>
 						<tbody>
 							<tr>
-								<td class="equipment">1</td>
-								<td class="equipment">2</td>
-								<td class="equipment">3</td>
-								<td class="equipment">4</td>
-								<td class="equipment">5</td>
-								<td class="equipment">6</td>
-								<td class="equipment">7</td>
-								<td class="equipment">8</td>
-								<td class="equipment">9</td>
-								<td class="equipment">10</td>
-								<td class="equipment">
-									<button type="button" class="btn btn-outline-success">Apply</button>
-								</td>
-								<td class="equipment">
-									<button type="button" class="btn btn-outline-danger">Delete</button>
-								</td>
+								<form>
+									<td class="equipment">Name</td>
+									<td class="equipment">
+										<input type="text" class="form-control iteminput" style="width: 40px" name="atk" id="atk"/>
+									</td>
+									<td class="equipment">
+										<input type="text" class="form-control iteminput" style="width: 40px" name="def" id="def"/>
+									</td>
+									<td class="equipment">
+										<input type="text" class="form-control iteminput" style="width: 40px" name="ref" id="ref"/>
+									</td>
+									<td class="equipment">
+										<input type="text" class="form-control iteminput" style="width: 40px" name="slots" id="slots"/>
+									</td>
+									<td class="equipment">
+										<input type="text" class="form-control iteminput" style="width: 40px" name="s1" id="s1"/>
+									</td>
+									<td class="equipment">
+										<input type="text" class="form-control iteminput" style="width: 40px" name="s2" id="s2"/>
+									</td>
+									<td class="equipment">
+										<input type="text" class="form-control iteminput" style="width: 40px" name="abil" id="abil"/>
+									</td>
+									<td class="equipment">
+										<input type="text" class="form-control iteminput" style="width: 40px" name="abilv" id="abilv"/>
+									</td>
+									<td class="equipment">
+										<input type="text" class="form-control iteminput" style="width: 40px" name="price" id="price"/>
+									</td>
+									<td class="equipment">
+										<button type="submit" class="btn btn-outline-success">Apply</button>
+									</td>
+									<td class="equipment">
+										<button type="submit" class="btn btn-outline-danger">Delete</button>
+									</td>
+								</form>
 							</tr>
 							<tr>
 								<td class="equipment">1</td>
