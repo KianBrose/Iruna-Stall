0dab6836eca570e9dc3ee6eb23bdb222b8a350fc
Kian Bröse Brito <45287789+17kibr@users.noreply.github.com>
2019-11-14 13:05:19 +0100

    Added errors

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -1,5 +1,30 @@
 @extends('errors::minimal')
 
-@section('title', __('Service Unavailable'))
+@section('title', __('Unauthorized'))
 @section('code', '503')
-@section('message', __($exception->getMessage() ?: 'Service Unavailable'))
+@section('message', __('Unauthorized'))
+
+<link href="{{ asset('css/error.css') }}" rel="stylesheet">
+<div class="container">
+	<div class="row">
+		<div class="col-md-11 offset-md-1" >
+		<div style="height: auto !important;">
+			<br>
+			<h4>Error!</h4>
+			<hr>
+			<div id="notfound">
+				<div class="notfound">
+					<div class="notfound-404">
+						<h1>Oops!</h1>
+					</div>
+					<h2>503 - Page not found</h2>
+					<p>The page you are looking for might have been removed had its name changed or is temporarily unavailable.</p>
+					<a href="/">Go To Homepage</a>
+				</div>
+			</div>
+		</div>
+	</div>
+
+
+
+@endsection
