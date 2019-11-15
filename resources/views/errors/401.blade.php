6f9f2aa167a00993333b151b37f5cffbd846928b
Kian Bröse Brito <45287789+17kibr@users.noreply.github.com>
2019-11-15 11:40:48 +0100

    Changed error pages

--- resources/views/errors/401.blade.php
+++ resources/views/errors/401.blade.php
@@ -17,8 +17,7 @@
 					<div class="notfound-404">
 						<h1>Oops!</h1>
 					</div>
-					<h2>401 - Page not found</h2>
-					<p>The page you are looking for might have been removed had its name changed or is temporarily unavailable.</p>
+					<p>{{ $exception->getMessage() }}</p>
 					<a href="/">Go To Homepage</a>
 				</div>
 			</div>
