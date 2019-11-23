60f713f1d5f05553a8a3ef8b3dc3e26ff63fbf0b
17kibr <17kibr@stockholmscience.se>
2019-11-23 12:12:10 +0100

    Update 404.blade.php

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,10 +1,8 @@
 @extends('errors::minimal')
 
-@section('title', __('Unauthorized'))
-@section('code', '404')
-@section('message', __('Unauthorized'))
+<link href="{{ asset('css/newerror.css') }}" rel="stylesheet">
+<script src="{{ asset('js/error.js') }}"></script>
 
-<link href="{{ asset('css/error.css') }}" rel="stylesheet">
 <div class="container">
 	<div class="row">
 		<div class="col-md-11 offset-md-1" >
@@ -12,18 +10,9 @@
 			<br>
 			<h4>Error!</h4>
 			<hr>
-			<div id="notfound">
-				<div class="notfound">
-					<div class="notfound-404">
-						<h1>Oops!</h1>
-					</div>
-					<p>{{ $exception->getMessage() }}</p>
-					<a href="/">Go To Homepage</a>
-				</div>
+			<div class="error-page">
+				<h1>404</h1>
+				<p>{{ $exception->getMessage() }}</p>
 			</div>
 		</div>
 	</div>
-
-
-
-@endsection
