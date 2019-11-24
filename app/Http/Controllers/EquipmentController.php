8bca585f1a4b0de812fb00d4b2d4b5cd56c72254
polowis <tranphuochung0912@gmail.com>
2019-11-24 20:16:17 +1100

    Update weapon edit function

--- app/Http/Controllers/EquipmentController.php
+++ app/Http/Controllers/EquipmentController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Equipment;
 use App\Http\Requests\StoreIrunaEquip;
+use App\Irunaitem;
 use RealRashid\SweetAlert\Facades\Alert;
 use Auth;
 
@@ -30,16 +31,43 @@ class EquipmentController extends Controller
                 }
 
                 if($this->validNumber(request('def'))){
-                    if(request('atk') < 70 && request('def') > 0){
+                    if((int)request('atk') < 70 && (int)request('def') > 0){
                         $item->atk = request('def');
                     }
                 }
-                $item->slots = request('slots');
-                $item->slot1 = request('slot1');
-                $item->slot2 = request('slot2');
-                $item->ability = request('ability');
-                $item->ability_level = request('ability_level');
-                $item->refinement = request('refinement');
+                $slot1 = Irunaitem::getItem(request('slot1'))->first();
+
+                if($slot1) {
+                    if($slot1->category == 'Crystas'){
+                        $item->slot1 = $slot1->name;
+                    }
+                   
+                }
+
+                $slot2 = Irunaitem::getItem(request('slot2'))->first();
+
+                if($slot2) {
+                    if($slot2->category == 'Crystas'){
+                        $item->slot2 = $slot2->name;
+                    }
+                    
+                }
+
+                $validAbi = Ability::where('type', request('ability'))->first();
+                if($validAbi){
+                    $item->ability = request('ability');
+                }
+                if($this->validNumber(request('ability_level'))){
+                    if((int)request('ability_level') >= 1 && (int)request('ability_level') <=5){
+                        $item->ability_level = request('ability_level');
+                    }
+                }
+                if($this->validNumber(request('refinement'))){
+                    if((int)request('refinement') >= 1 && (int)request('refinement') <= 9){
+                        $item->refinement = request('refinement');
+                    }
+                        
+                }
                 $item->save();
                 Alert::toast('successfully edited an item', 'success');
                 return redirect('/viewitem');
