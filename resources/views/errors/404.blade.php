dace6f958cd5d783b55e86c60dfbdbd0c03cf04d
17kibr <17kibr@stockholmscience.se>
2019-11-24 13:44:50 +0100

    updated all error pages

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -4,8 +4,8 @@
 <main>
   <div>
     <div>
-      <span>404&nbsp;error</span>
-      <span>page&nbsp;not&nbsp;found</span>
+      <span>404 error</span>
+      <span>{{ $exception->getMessage() }}</span>
     </div>
     <svg viewBox="0 0 200 600">
       <polygon points="118.302698 8 59.5369448 66.7657528 186.487016 193.715824 14 366.202839 153.491505 505.694344 68.1413353 591.044514 200 591.044514 200 8"></polygon>
@@ -19,10 +19,10 @@
       <polygon points="118.302698 8 59.5369448 66.7657528 186.487016 193.715824 14 366.202839 153.491505 505.694344 68.1413353 591.044514 0 591.044514 0 8"></polygon>
     </svg>
     <div>
-      <span>sorry&nbsp;about&nbsp;that!</span>
+      <span>sorry about that!</span>
       <span>
         <a>
-          <b>return&nbsp;home?</b>
+          <b>return home?</b>
         </a>
       </span>
     </div>
