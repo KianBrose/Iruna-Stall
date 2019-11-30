7012e59d6a37e2b618cf6465fb4fe9cacd9da207
polowis <tranphuochung0912@gmail.com>
2019-11-30 11:23:41 +1100

    update admin controller

--- app/Http/Controllers/IrunaitemController.php
+++ app/Http/Controllers/IrunaitemController.php
@@ -9,6 +9,9 @@ use App\Irunaitem;
 class IrunaitemController extends Controller
 {
     const Equipment = array('Claws, Staff, Swords, Throws, Bows, Armors, Additional, Special');
+    const Xtal = 'Crystas';
+    const Relic = 'RelicCrystas';
+    const AL = 'Alcrystas';
     public function __construct()
     {
         $this->middleware('isAdmin');
@@ -17,11 +20,37 @@ class IrunaitemController extends Controller
     public function add(Request $request){
         $type = request('type');
         $irunaitem = new Irunaitem();
-        if(in_array($irunaitem, IrunaitemController::Equipment)){
+        if(in_array($type, IrunaitemController::Equipment)){
             $irunaitem->category = request('type');
             $irunaitem->name = request('name');
             $irunaitem->save();
         }
+
+        if($type == IrunaitemController::Xtal){
+            $irunaitem->category = 'Crystas';
+            $irunaitem->name = '◇'.request('name');
+            $irunaitem->save();
+        }
+
+        if($type == IrunaitemController::Relic){
+            $irunaitem->category = 'RelicCrystas';
+            $irunaitem->name = '□'.request('name');
+            $irunaitem->save();
+        }
+
+        if($type == IrunaitemController::AL){
+            $irunaitem->category = 'AlCrystas';
+            $irunaitem->name = '▲'.request('name');
+            $irunaitem->save();
+        }
+        else{
+            $irunaitem->category = request('type');
+            $irunaitem->name = request('name');
+            $irunaitem->save();
+        }
+
+
+
         
     }
 }
