280db7906c423aafd901ab6a98f400523043d2ac
polowis <tranphuochung0912@gmail.com>
2019-12-01 11:05:47 +1100

    remove debug message

--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -4,8 +4,8 @@
 <main>
   <div>
     <div>
-      <span>500 error</span>
-      <span>{{ $exception->getMessage() }}</span>
+      <span>500</span>
+      <span></span>
     </div>
     <svg viewBox="0 0 200 600">
       <polygon points="118.302698 8 59.5369448 66.7657528 186.487016 193.715824 14 366.202839 153.491505 505.694344 68.1413353 591.044514 200 591.044514 200 8"></polygon>
