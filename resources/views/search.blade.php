7511f5a2181ee952825f5ad7b7c70de3477cb43c
polowis <tranphuochung0912@gmail.com>
2019-12-02 15:35:24 +1100

    change http to https

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -233,7 +233,7 @@
 			   
 				source: function(request, response) {
 					$.ajax({
-					url: "{{url('api/searchitem')}}",
+					url: "{{secure_url('api/searchitem')}}",
 					data: {
 							term : request.term
 					 },
