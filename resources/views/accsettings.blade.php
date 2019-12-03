374776b1efc38c58cff7d57e1a04eec90a8b6ab2
polowis <tranphuochung0912@gmail.com>
2019-12-03 11:24:35 +1100

    add meta tag for facebook

--- resources/views/accsettings.blade.php
+++ resources/views/accsettings.blade.php
@@ -6,7 +6,9 @@
 	<meta name="robots" content="index, follow"/>
 	<meta name="description" content="Find, buy and sell items for free on the Iruna Online Global Stall! In the Global Stall, you can post your items as a seller and search for items as a buyer. The stall is free to use and requires no account to search for items."/>
 	<meta name="keywords" content="Iruna, Stall, Global, iruna, stall, global"/>
-	
+    <meta property="og:image" content="https://irunastall.com/img/iruna.jpg">
+    <meta property="og:title" content="Iruna Stall">
+    <meta property="og:description" content="Find, buy and sell items for free on the Iruna Online Global Stall! In the Global Stall, you can post your items as a seller and search for items as a buyer. The stall is free to use and requires no account to search for items.">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="hostname" content="irunastall.com"/>
     <meta name="csrf-token" content="{{ csrf_token() }}">
