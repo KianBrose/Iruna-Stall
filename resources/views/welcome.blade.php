54ef5248d28c427018a21238961fb9dde760a1ef
Abhishek Kumar <akumar@hotmail.co.in>
2019-12-03 17:27:20 +0530

    update main page with SEO meta tags

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,17 +3,35 @@
 
 <head>
     <meta charset="utf-8">
+    <title>Iruna Global Stall | Iruna Online</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta property="og:image" content="https://irunastall.com/img/iruna.jpg">
+    <meta name="robots" content="index, follow" >
+    <meta name="description" content="Iruna Stall is a platform to find, buy and sell items for free! In the Iruna Global Stall, you can post your items as a seller and search for items as a buyer. The stall is free to use and requires no account to search for items.">
+    <meta name="keywords" content="Iruna, Stall, Global, iruna, stall, global, Iruna Online, buy, sell, trade" >
+    <meta name="hostname" content="irunastall.com" >
+    <meta name='subject' content="Iruna Global Stall">
+    <meta name="author" content="Kian Brose" >
+    <meta name='coverage' content='Worldwide'>
+    <meta name='distribution' content='Global'>
+    <meta name='rating' content='General'>
+    <meta name='HandheldFriendly' content='True'>
+    <meta name='MobileOptimized' content='320'>
+    <meta name='target' content='all'>
+    <meta name="apple-mobile-web-app-title" content="Iruna Global Stall">
+    <meta name='apple-mobile-web-app-capable' content='yes'>
+    <meta name='apple-touch-fullscreen' content='yes'>
+    <meta name='apple-touch-fullscreen' content='yes'>
+    <meta name='application-name' content="Iruna Global Stall">
+    <meta name="twitter:card" content="summary" >
+    <meta name="twitter:title" content="Iruna Global Stall" >
+    <meta name="twitter:description" content="Iruna global stall is platform to find, buy and sell items of Iruna online game. Account is not needed for searching for items" >
     <meta property="og:title" content="Iruna Stall">
+    <meta property="og:type" content="website" >
     <meta property="og:description" content="Find, buy and sell items for free on the Iruna Online Global Stall! In the Global Stall, you can post your items as a seller and search for items as a buyer. The stall is free to use and requires no account to search for items.">
-	<meta name="robots" content="index, follow"/>
-	<meta name="description" content="Find, buy and sell items for free on the Iruna Online Global Stall! In the Global Stall, you can post your items as a seller and search for items as a buyer. The stall is free to use and requires no account to search for items."/>
-    <meta name="keywords" content="Iruna, Stall, Global, iruna, stall, global"/>
-    <meta name="hostname" content="irunastall.com"/>
+    <meta property="og:image" content="https://irunastall.com/img/iruna.jpg">
+    <meta property="og:url" content="https://irunastall.com" >
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>Iruna Global Stall</title>
-
+    <link rel="canonical" href="https://irunastall.com" >
     <!-- bootstrap css -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <!-- Fonts -->
