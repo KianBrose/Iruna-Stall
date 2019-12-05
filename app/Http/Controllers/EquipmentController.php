f7fdcec4d4550a865c79932774c4ef4c7264cbd3
Kian Bröse Brito <45287789+17kibr@users.noreply.github.com>
2019-12-05 13:07:06 +0100

    Update EquipmentController.php

--- app/Http/Controllers/EquipmentController.php
+++ app/Http/Controllers/EquipmentController.php
@@ -113,9 +113,13 @@ class EquipmentController extends Controller
                 }
 
                 if($this->validNumber(request('ability_level'))){
-                    if((int)request('ability_level') >= 1 && (int)request('ability_level') <=5){
-                        $item->ability_level = request('ability_level');
-                    }
+					if(request('ability') != null or request('ability') != ""){
+						if((int)request('ability_level') >= 1 && (int)request('ability_level') <=5){
+                        $item->ability_level = request('ability_level');}
+					}
+					else{
+						$item->ability_level = 0;
+					}
                 }
 
                 if($this->validNumber(request('refinement'))){
