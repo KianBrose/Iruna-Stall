48ccbaeec6dca5397c0f8c70914360859cf66149
polowis <tranphuochung0912@gmail.com>
2019-12-05 15:51:19 +1100

    change limit to 10 item can be displayed

--- app/Http/Controllers/AccountController.php
+++ app/Http/Controllers/AccountController.php
@@ -37,11 +37,11 @@ class AccountController extends Controller
     public function getUserId($id){
         $user = User::where('user_id', $id)->firstOrFail();
         if($user != null){
-            $equipSearch = Equipment::where('owner_id', $id)->paginate(2, ['*'], 'equipPage');
-            $xtalSearch = Xtal::where('owner_id', $id)->paginate(2, ['*'], 'xtalPage');
-            $itemSearch = Items::where('owner_id', $id)->paginate(2, ['*'], 'itemPage');
-            $alSearch = Ai::where('owner_id', $id)->paginate(2, ['*'], 'alPage');
-            $relicSearch = Relic::where('owner_id', $id)->paginate(2, ['*'], 'relicPage');
+            $equipSearch = Equipment::where('owner_id', $id)->paginate(10, ['*'], 'equipPage');
+            $xtalSearch = Xtal::where('owner_id', $id)->paginate(10, ['*'], 'xtalPage');
+            $itemSearch = Items::where('owner_id', $id)->paginate(10, ['*'], 'itemPage');
+            $alSearch = Ai::where('owner_id', $id)->paginate(10, ['*'], 'alPage');
+            $relicSearch = Relic::where('owner_id', $id)->paginate(10, ['*'], 'relicPage');
             return view('seller', compact('user', 'equipSearch', 'xtalSearch', 'itemSearch', 'alSearch', 'relicSearch'));
         }
         
