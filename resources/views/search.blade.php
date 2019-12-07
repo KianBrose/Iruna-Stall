2da0bf1470141cd5ee5d5fbcb3afad70b400a63c
17kibr <17kibr@stockholmscience.se>
2019-12-07 12:14:51 +0100

    Update search.blade.php

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -286,14 +286,14 @@
 			window.onbeforeunload = function(e){    
 				window.name += ' [' + $(window).scrollTop().toString() + '[' + $(window).scrollLeft().toString();
 			};
-				$.maintainscroll = function() {
+				$.maintainscroll = setTimeout(function() {
 				if(window.name.indexOf('[') > 0)
 				{
 				var parts = window.name.split('['); 
 				window.name = $.trim(parts[0]);
 				window.scrollTo(parseInt(parts[parts.length - 1]), parseInt(parts[parts.length - 2]));
 				}   
-			};  
+			}, 100) 
 			$.maintainscroll();
 		})(jQuery);
 		</script>
