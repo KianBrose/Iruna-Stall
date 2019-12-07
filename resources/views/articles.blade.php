cd1ffce096dca99a48719e8240ba824b1634b57b
17kibr <17kibr@stockholmscience.se>
2019-12-07 15:24:24 +0100

    added tutorial and article page

--- resources/views/articles.blade.php
+++ resources/views/articles.blade.php
@@ -3,13 +3,13 @@
 
 <head>
     <meta charset="utf-8">
-    <title>Iruna Global Stall | Iruna Online</title>
+    <title> Guides | Iruna Global Stall</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="robots" content="index, follow" >
-    <meta name="description" content="Iruna Stall is a platform to find, buy and sell items for free! In the Iruna Global Stall, you can post your items as a seller and search for items as a buyer. The stall is free to use and requires no account to search for items.">
-    <meta name="keywords" content="Iruna, Stall, Global, iruna, stall, global, Iruna Online, buy, sell, trade, Iruna enchanter skills, Iruna struggler, Iruna monk, Iruna online classes, samurai, ninja, leveling, dragon, crystal, quest, Toram online" >
+    <meta name="description" content="Find about Iruna Stall on how it used to find, buy and sell items for free! In the Iruna Global Stall, you can post your items as a seller and search for items without the need for registration.">
+    <meta name="keywords" content="Iruna, Stall, Global, iruna, stall, global, Iruna Online, buy, sell, trade, about, Kian Brose, Kumar, Polowis, terms, service, contact" >
     <meta name="hostname" content="irunastall.com" >
-    <meta name='subject' content="Iruna Global Stall">
+    <meta name='subject' content="About Iruna Global Stall">
     <meta name="author" content="Kian Brose" >
     <meta name='coverage' content='Worldwide'>
     <meta name='distribution' content='Global'>
@@ -17,31 +17,29 @@
     <meta name='HandheldFriendly' content='True'>
     <meta name='MobileOptimized' content='320'>
     <meta name='target' content='all'>
-    <meta name="apple-mobile-web-app-title" content="Iruna Global Stall">
+    <meta name="apple-mobile-web-app-title" content="About | Iruna Global Stall">
     <meta name='apple-mobile-web-app-capable' content='yes'>
     <meta name='apple-touch-fullscreen' content='yes'>
     <meta name='apple-touch-fullscreen' content='yes'>
     <meta name='application-name' content="Iruna Global Stall">
     <link rel="apple-touch-icon" href="https://irunastall.com/img/iruna.jpg">
     <meta name="twitter:card" content="summary" >
-    <meta name="twitter:title" content="Iruna Global Stall" >
-    <meta name="twitter:description" content="Iruna global stall is platform to find, buy and sell items of Iruna online game. Account is not needed for searching for items" >
+    <meta name="twitter:title" content="About | Iruna Global Stall" >
+    <meta name="twitter:description" content="Know about Iruna global stall. This is a platform to find, buy and sell items of Iruna online game. Account is not needed for searching for items" >
     <meta property="og:title" content="Iruna Stall">
     <meta property="og:type" content="website" >
-    <meta property="og:description" content="Find, buy and sell items for free on the Iruna Online Global Stall! In the Global Stall, you can post your items as a seller and search for items as a buyer. The stall is free to use and requires no account to search for items.">
+    <meta property="og:description" content="Know about on how to find, buy and sell items for free on the Iruna Online Global Stall! In the Global Stall, you can post your items as a seller and search for items as a buyer. The stall is free to use and requires no account to search for items.">
     <meta property="og:image" content="https://irunastall.com/img/iruna.jpg">
     <meta property="og:url" content="https://irunastall.com" >
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
 
     <link rel="canonical" href="https://irunastall.com" >
+
     <!-- bootstrap css -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-    <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
-    <link href="css/style.css" rel="stylesheet">
-	
 	<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
 	<script>
 		 (adsbygoogle = window.adsbygoogle || []).push({
@@ -49,11 +47,9 @@
 			  enable_page_level_ads: true
 		 });
 	</script>
-
 </head>
 
 <body>
-        
 <div id="app">
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
             <div class="container">
@@ -102,7 +98,7 @@
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                <a class="dropdown-item" href="/user/{{ auth()->user()->user_id }}">Profile</a>
+                                    <a class="dropdown-item" href="/user/{{ auth()->user()->user_id }}">Profile</a>
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
@@ -120,42 +116,20 @@
             </div>
         </nav>
     </div>
-
     <div class="container">
         <div class="row" >
             <div class="col-md-7 offset-md-3" >
 
                 <div style="height: auto !important;">
-		    <br>
-                    <h4>Welcome!</h4>
+		    		<br>
+                    <h4>Guides</h4>
                     <hr>
-					
-					<form class="form-inline" name="form1" id="form1" action="/search" method="GET">
-						@csrf
-					  <div class="form-group mb-2" style="width:80%">
-						<label for="search" class="sr-only">Item name</label>
-						<input type="text" class="form-control ui-autocomplete-input" id="search"  name="search" placeholder="Item/Ability name" autocomplete="off" style="width:98%">
-					  </div>
-					  <button type="submit" class="btn btn-primary mb-2" name="search_button" id="search_button" value="Search" style="width:20%">Search</button>
-					</form>
-                    @error('searcherror')
-                    <div style="color:red">{{$message}}</div>
-                    <br>
-                    @enderror
-                    <div class="row divider" role="separator" ></div>
-					<br>
-                    <p style="font-size: 14px;">Welcome to our Iruna Global Stall</p>
-					<p style="font-size: 14px;">Enter the item name and press search</p>
-					<form class="form-inline" name="form1" id="form1" action="/search" method="GET">
-							<button type="submit" class="btn btn-primary mb-2" name="search_button" id="search_button" value="Search" style="width:100%">Browse all items</button>
-					</form>
-                    <form class="form-inline" name="form1" id="form1" action="/scammers" method="GET">
-							<button type="submit" class="btn btn-primary mb-2" value="Search" style="width:100%">View scammer list</button>
-					</form>
-                    <form class="form-inline" name="form1" id="form1" action="/tutorial" method="GET">
-							<button type="submit" class="btn btn-primary mb-2" value="Search" style="width:100%; background-color: green; ">Iruna stall tutorial</button>
-					</form>
+                    <div>
+						<p>Here you can find recently updated guides for each individual class in the game:</p>
+                        <br>
+					</div>
                     <hr>
+                        
                     <div class="row divider" role="separator" style="margin-top:20px;"></div>
                 </div>
                 <div></div>
@@ -164,39 +138,12 @@
     </div>
     @include('sweetalert::alert')
     <!-- bootstrap js -->
-    <script src="https://code.jquery.com/jquery-3.4.1.min.js"   integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="   crossorigin="anonymous"></script>
-	<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js"   integrity="sha256-VazP97ZCwtekAsvgPBSUwPFKdrwD3unUfSGVYrahUqU="   crossorigin="anonymous"></script>
-    
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-    <script type="text/javascript" id="cookieinfo"
+<script type="text/javascript" id="cookieinfo"
     src="//cookieinfoscript.com/js/cookieinfo.min.js">
-	</script>
-    <script>
-        $(document).ready(function() {
-            
-        $( "#search" ).autocomplete({
-       
-        source: function(request, response) {
-            $.ajax({
-            url: "{{secure_url('api/searchitem')}}",
-            data: {
-                    term : request.term
-             },
-            dataType: "json",
-            success: function(data){
-               var resp = $.map(data,function(obj){
-                    return obj.name;
-               }); 
- 
-               response(resp);
-            }
-        });
-    },
-    minLength: 3
- });
-});
-        </script>
+</script>
 </body>
 
 </html>
--- resources/views/tutorial.blade.php
+++ resources/views/tutorial.blade.php
@@ -3,13 +3,13 @@
 
 <head>
     <meta charset="utf-8">
-    <title>Iruna Global Stall | Iruna Online</title>
+    <title> Tutorial | Iruna Global Stall</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="robots" content="index, follow" >
-    <meta name="description" content="Iruna Stall is a platform to find, buy and sell items for free! In the Iruna Global Stall, you can post your items as a seller and search for items as a buyer. The stall is free to use and requires no account to search for items.">
-    <meta name="keywords" content="Iruna, Stall, Global, iruna, stall, global, Iruna Online, buy, sell, trade, Iruna enchanter skills, Iruna struggler, Iruna monk, Iruna online classes, samurai, ninja, leveling, dragon, crystal, quest, Toram online" >
+    <meta name="description" content="Find about Iruna Stall on how it used to find, buy and sell items for free! In the Iruna Global Stall, you can post your items as a seller and search for items without the need for registration.">
+    <meta name="keywords" content="Iruna, Stall, Global, iruna, stall, global, Iruna Online, buy, sell, trade, about, Kian Brose, Kumar, Polowis, terms, service, contact" >
     <meta name="hostname" content="irunastall.com" >
-    <meta name='subject' content="Iruna Global Stall">
+    <meta name='subject' content="About Iruna Global Stall">
     <meta name="author" content="Kian Brose" >
     <meta name='coverage' content='Worldwide'>
     <meta name='distribution' content='Global'>
@@ -17,31 +17,29 @@
     <meta name='HandheldFriendly' content='True'>
     <meta name='MobileOptimized' content='320'>
     <meta name='target' content='all'>
-    <meta name="apple-mobile-web-app-title" content="Iruna Global Stall">
+    <meta name="apple-mobile-web-app-title" content="About | Iruna Global Stall">
     <meta name='apple-mobile-web-app-capable' content='yes'>
     <meta name='apple-touch-fullscreen' content='yes'>
     <meta name='apple-touch-fullscreen' content='yes'>
     <meta name='application-name' content="Iruna Global Stall">
     <link rel="apple-touch-icon" href="https://irunastall.com/img/iruna.jpg">
     <meta name="twitter:card" content="summary" >
-    <meta name="twitter:title" content="Iruna Global Stall" >
-    <meta name="twitter:description" content="Iruna global stall is platform to find, buy and sell items of Iruna online game. Account is not needed for searching for items" >
+    <meta name="twitter:title" content="About | Iruna Global Stall" >
+    <meta name="twitter:description" content="Know about Iruna global stall. This is a platform to find, buy and sell items of Iruna online game. Account is not needed for searching for items" >
     <meta property="og:title" content="Iruna Stall">
     <meta property="og:type" content="website" >
-    <meta property="og:description" content="Find, buy and sell items for free on the Iruna Online Global Stall! In the Global Stall, you can post your items as a seller and search for items as a buyer. The stall is free to use and requires no account to search for items.">
+    <meta property="og:description" content="Know about on how to find, buy and sell items for free on the Iruna Online Global Stall! In the Global Stall, you can post your items as a seller and search for items as a buyer. The stall is free to use and requires no account to search for items.">
     <meta property="og:image" content="https://irunastall.com/img/iruna.jpg">
     <meta property="og:url" content="https://irunastall.com" >
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
 
     <link rel="canonical" href="https://irunastall.com" >
+
     <!-- bootstrap css -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-    <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
-    <link href="css/style.css" rel="stylesheet">
-	
 	<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
 	<script>
 		 (adsbygoogle = window.adsbygoogle || []).push({
@@ -49,11 +47,9 @@
 			  enable_page_level_ads: true
 		 });
 	</script>
-
 </head>
 
 <body>
-        
 <div id="app">
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
             <div class="container">
@@ -102,7 +98,7 @@
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                <a class="dropdown-item" href="/user/{{ auth()->user()->user_id }}">Profile</a>
+                                    <a class="dropdown-item" href="/user/{{ auth()->user()->user_id }}">Profile</a>
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
@@ -120,42 +116,56 @@
             </div>
         </nav>
     </div>
-
     <div class="container">
         <div class="row" >
             <div class="col-md-7 offset-md-3" >
 
                 <div style="height: auto !important;">
-		    <br>
-                    <h4>Welcome!</h4>
+		    		<br>
+                    <h4>Iruna stall tutorial</h4>
                     <hr>
-					
-					<form class="form-inline" name="form1" id="form1" action="/search" method="GET">
-						@csrf
-					  <div class="form-group mb-2" style="width:80%">
-						<label for="search" class="sr-only">Item name</label>
-						<input type="text" class="form-control ui-autocomplete-input" id="search"  name="search" placeholder="Item/Ability name" autocomplete="off" style="width:98%">
-					  </div>
-					  <button type="submit" class="btn btn-primary mb-2" name="search_button" id="search_button" value="Search" style="width:20%">Search</button>
-					</form>
-                    @error('searcherror')
-                    <div style="color:red">{{$message}}</div>
-                    <br>
-                    @enderror
-                    <div class="row divider" role="separator" ></div>
-					<br>
-                    <p style="font-size: 14px;">Welcome to our Iruna Global Stall</p>
-					<p style="font-size: 14px;">Enter the item name and press search</p>
-					<form class="form-inline" name="form1" id="form1" action="/search" method="GET">
-							<button type="submit" class="btn btn-primary mb-2" name="search_button" id="search_button" value="Search" style="width:100%">Browse all items</button>
-					</form>
-                    <form class="form-inline" name="form1" id="form1" action="/scammers" method="GET">
-							<button type="submit" class="btn btn-primary mb-2" value="Search" style="width:100%">View scammer list</button>
-					</form>
-                    <form class="form-inline" name="form1" id="form1" action="/tutorial" method="GET">
-							<button type="submit" class="btn btn-primary mb-2" value="Search" style="width:100%; background-color: green; ">Iruna stall tutorial</button>
-					</form>
+                    <div>
+						<h4>Buyer tutorial &amp; How it works:</h4>
+                        <br>
+                        <p>As a buyer, you do not need to register an account to search for items.
+                        <br>
+                            To search for an item, simply go to our main page and enter the item name of the item you want to search for. You can also press the "<a href="https://irunastall.com/search?search_button=Search"><b>Browse All</b></a>" button to view all items we currently have.
+                        <br>
+                            Once you found the item you want, press/click on the buyer's name in the <b>Contact</b> row to go to the seller's profile. You will find his/her contact information there so you can negotiate and arrange a place to trade.
+                        </p>
+                        <p>To search for <b>abilities inside equipment</b>, enter the ability name into the search bar and press <a href="/"><b>Search</b></a>. Please note that <b>the autofill function will not suggest ability names, so the abilities have to be entered manually</b>.</p>
+                        <p>Searching for XTALS will also display equipment with the XTAL inside it</p>
+					</div>
                     <hr>
+                    <div>
+                        <h4>Seller tutorial</h4>
+                        <br>
+                        <p>As a seller, <b>you need to <a href="/register">register an account</a> to be able to post items in our database</b>.</p>
+                        <p>Once you have created your account, go to the <a href="/account"><b>Account</b></a> page to enter your contact information. If a buyer is interested in any of your items you will get contacted through our 2 available methods: Facebook and Discord (We plan on adding more later).</p>
+                        <p>If you have entered contact information, please head to the <a href="/additem"><b>Add item</b></a> page to be able to add items of any category.</p>
+                        <p>Please note that the prices have to be entered in full numerical values without symbols or spaces, example: 750000000</p>
+                        <p>Please enter the price <b>per item</b> when adding Materials, Xtals, ALs and Relics</p>
+                    </div>
+                    
+                    <hr>
+                    <table>
+                      <tr>
+                        <th>Scammer ID</th>
+                        <th>IGN</th>
+                        <th>ID</th>
+                        <th>Facebook Name</th>
+                        <th>Facebook Profile</th>
+                        <th>Discord Name</th>
+                      </tr>
+                      <tr>
+                        <td>1</td>
+                        <td>?</td>
+                        <td>?</td>
+                        <td>?</td>
+                        <td>?</td>
+                        <td>?</td>
+                      </tr>
+                    </table>
                     <div class="row divider" role="separator" style="margin-top:20px;"></div>
                 </div>
                 <div></div>
@@ -164,39 +174,12 @@
     </div>
     @include('sweetalert::alert')
     <!-- bootstrap js -->
-    <script src="https://code.jquery.com/jquery-3.4.1.min.js"   integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="   crossorigin="anonymous"></script>
-	<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js"   integrity="sha256-VazP97ZCwtekAsvgPBSUwPFKdrwD3unUfSGVYrahUqU="   crossorigin="anonymous"></script>
-    
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-    <script type="text/javascript" id="cookieinfo"
+<script type="text/javascript" id="cookieinfo"
     src="//cookieinfoscript.com/js/cookieinfo.min.js">
-	</script>
-    <script>
-        $(document).ready(function() {
-            
-        $( "#search" ).autocomplete({
-       
-        source: function(request, response) {
-            $.ajax({
-            url: "{{secure_url('api/searchitem')}}",
-            data: {
-                    term : request.term
-             },
-            dataType: "json",
-            success: function(data){
-               var resp = $.map(data,function(obj){
-                    return obj.name;
-               }); 
- 
-               response(resp);
-            }
-        });
-    },
-    minLength: 3
- });
-});
-        </script>
+</script>
 </body>
 
 </html>
