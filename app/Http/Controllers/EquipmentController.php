d2503e62117580cc11267b67642c90d1299979bf
polowis <tranphuochung0912@gmail.com>
2019-12-08 15:41:50 +1100

    add shortcut price to equipment  edit function

--- app/Http/Controllers/EquipmentController.php
+++ app/Http/Controllers/EquipmentController.php
@@ -9,6 +9,7 @@ use App\Irunaitem;
 use RealRashid\SweetAlert\Facades\Alert;
 use Auth;
 use App\Ability;
+use App\Http\Requests\StoreIrunaItem;
 
 class EquipmentController extends Controller
 {
@@ -23,17 +24,29 @@ class EquipmentController extends Controller
             else{
                 $xtal1 = request('slot1');
                 $xtal2 = request('slot2');
-                if($this->validNumber(request('price'))){
-                    if(strlen(request('price')) > 12){
-                        $item->price = 999999999999;
+                
+                if(strlen(request('price')) > 12){
+                    $item->price = 999999999999;
+                }
+                else if(strlen(request('price')) < 1){
+                    $item->price = 1;
+                } else{
+                    if(in_array(strtolower(substr(request('price'), -1)), StoreIrunaItem::PriceType)){
+                        $priceDenote = strtolower(substr(request('price'), -1));
+                        $priceNumber = substr(request('price'), 0, -1);
+                        try{
+                            if($this->validConvertPrice($priceDenote, $priceNumber)){
+                                $price = $this->convertPrice($priceDenote, $priceNumber);
+                                $item->price = $price;
+                            } 
+                        } catch(Exception $e){
+            
+                        }
                     }
-                    else if((int)request('price') < 1){
-                        $item->price = 1;
-                    } else{
+                    if(is_numeric(request('price'))){
                         $item->price = request('price');
-                    }
-                    
-                } 
+                    } 
+                }
 
                 if($this->validNumber(request('atk'))){
                     
@@ -163,6 +176,99 @@ class EquipmentController extends Controller
             return false;
         }
         
+    }
+
+        /**
+     * 
+     * check if the price is valid
+     * @return bool
+     */
+    public function validConvertPrice($priceDenote, $priceNumber){
+        if($priceDenote == 'b'){
+            
+            if($this->validBillionPrice($priceDenote, $priceNumber)){
+                return true;
+            }else{
+                return false;
+            }
+            
+        }
+        if($priceDenote == 'm'){
+            
+            if($this->validMillionPrice($priceDenote, $priceNumber)){
+                return true;
+            } else{
+                return false;
+            }
+        }
+
+        if($priceDenote == 'k'){
+            if($this->validThousandsPrice($priceDenote, $priceNumber)){
+                return true;
+            } else{
+                return false;
+            }
+        }
+    }
+    
+    /**
+     * Check for valid billion price
+     * @return bool
+     */
+    private function validBillionPrice($priceDenote, $priceNumber){
+        if(is_numeric($priceNumber)){
+                
+            if((int)$priceNumber * 1000000000 > 999999999999){
+                return false;
+            } else{
+                
+                return true;
+            }
+        } else{
+            return false;
+        }
+    }
+    /**
+     * check for valid millions price
+     */
+
+    private function validMillionPrice($priceDenote, $priceNumber){
+        if(is_numeric($priceNumber)){
+            if((int)$priceNumber * 1000000 > 999999999999){
+                return false;
+            } else{
+                return true;
+            }
+        } else{
+            return false;
+        }
+    }
+
+    /**
+     * Check for valid thousands price
+     */
+    private function validThousandsPrice($priceDenote, $priceNumber){
+        if(is_numeric($priceNumber)){
+            if((int)$priceNumber * 1000 > 999999999999){
+                return false;
+            } else{
+                return true;
+            }
+        } else{
+            return false;
+        }
+    }
+
+    public function convertPrice($priceDenote, $priceNumber){
+        if($priceDenote == 'b'){
+            return (int)$priceNumber * 1000000000;
+        } 
+        if($priceDenote == 'm'){
+           return (int)$priceNumber * 1000000;
+        }
+        if($priceDenote == 'k'){
+            return (int)$priceNumber * 1000;
+        }
     }
     
 }
