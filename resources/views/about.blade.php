5c04ea9101e310bbab6e8e2a9f5cd9eb2013bfa8
polowis <tranphuochung0912@gmail.com>
2019-12-10 18:46:38 +1100

    update

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -99,6 +99,7 @@
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
                                     <a class="dropdown-item" href="/user/{{ auth()->user()->user_id }}">Profile</a>
+                                    <a class="dropdown-item" href="/account">Settings</a>
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
--- resources/views/seller.blade.php
+++ resources/views/seller.blade.php
@@ -96,6 +96,8 @@
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="/user/{{ auth()->user()->user_id }}">Profile</a>
+                                    <a class="dropdown-item" href="/account">Settings</a>
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
