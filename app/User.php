cd5dfa4d3ef699de911e8a2ea39bb0b0ff73ee39
polowis <tranphuochung0912@gmail.com>
2019-12-28 16:28:14 +1100

    hide admin field

--- app/User.php
+++ app/User.php
@@ -30,7 +30,7 @@ class User extends Authenticatable implements MustVerifyEmail
      * @var array
      */
     protected $hidden = [
-        'password', 'remember_token', 'api_token'
+        'password', 'remember_token', 'api_token', 'isAdmin', 'activate'
     ];
 
     /**
