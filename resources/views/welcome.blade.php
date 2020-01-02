5408f84451dc35966c8a9709f931f4b61f8f3826
Kian Bröse Brito <45287789+17kibr@users.noreply.github.com>
2020-01-02 02:00:34 +0100

    made weight to 900 for slightly bolder than bold effect

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -92,7 +92,7 @@
                             @endif
                         @else
                             <li class="nav-item dropdown notification">
-                                <span class="badge">3</span>
+                                <span class="badge" style="font-weight: 900;">3</span>
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }} <span class="caret"></span>
                                 </a>
@@ -102,7 +102,7 @@
                                 <a class="dropdown-item" href="/account">Settings</a>
                                 <a href="/private" class="dropdown-item">
                                   <span>Messages</span>
-                                  <span style="color: red;">3</span>
+                                  <span style="color: red; font-weight: 900;">3</span>
                                 </a>
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
