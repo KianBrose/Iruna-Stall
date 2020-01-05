6730d8f8c0c5578bbca4df5ff356af21121c5a52
Kian Bröse Brito <45287789+17kibr@users.noreply.github.com>
2020-01-05 20:46:15 +0100

    added banner to main page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -53,8 +53,9 @@
 <div id="app">
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
             <div class="container">
+				
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    Iruna Global Stall
+						<img alt="Iruna Stall" src="{{asset('img/bannerIS.png')}}">
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
