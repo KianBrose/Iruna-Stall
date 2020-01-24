e9c2c158a4becc6b92c1b16ad895f737b5f35b23
polowis <tranphuochung0912@gmail.com>
2020-01-25 10:36:23 +1100

    fix: Rename `encrypted` to `forceTLS` for Pusher
    I just realized that encrypted option for pusher is deprecated.

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -45,5 +45,5 @@ window.Echo = new Echo({
     broadcaster: 'pusher',
     key: process.env.MIX_PUSHER_APP_KEY,
     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
-    encrypted: true
+    forceTLS: true
 });
