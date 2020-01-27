a2b7bb24b3fdee9790dff24c9361d205283b3322
polowis <tranphuochung0912@gmail.com>
2020-01-27 13:12:14 +1100

    feat: improve middleware function to validate ip

--- app/Traits/IpCheckTrait.php
+++ app/Traits/IpCheckTrait.php
@@ -2,6 +2,7 @@
 
 namespace App\Traits;
 
+use App\Models\Blocker;
 use Illuminate\Support\Facades\Request;
 use App\Traits\IpAddressTrait;
 
@@ -19,6 +20,46 @@ trait IpCheckTrait
         $route = Request::route();
         $ipAddressDetails = $this->checkIP($ip);
 
+        $block = false;
+
+        $block = $this->isBlocked($ip);
+
+        $this->getAction($block);
+
+
+    }
+
+    private function getAction($block)
+    {
+        if($block)
+        {
+            if(auth()->check())
+            {
+                auth()->logout();
+                return redirect('/login');
+            }
+        }
+    }
+
+
+    /**
+     * 
+     * check if ip is not blocked
+     * 
+     * @param string $check
+     * 
+     * @return bool
+     */
+    private function isBlocked($check)
+    {
+        $blockedData = Blocker::all();
+
+        foreach($blockedData as $data){
+            if($data->value == $check){
+                return true;
+            }
+        }
+        return false;
     }
 }
 ?>
