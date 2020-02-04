38c121b0e0011a6ad6583df2c05cb92264cf3baa
polowis <tranphuochung0912@gmail.com>
2020-02-04 11:33:42 +1100

    feat: create function to render wiki page

--- app/Http/Controllers/Wiki/WikiController.php
+++ app/Http/Controllers/Wiki/WikiController.php
@@ -1,7 +1,7 @@
 <?php
 namespace App\Http\Controllers\Admin;
 
-
+use App\Message;
 use App\Http\Controllers\Controller;
 
 class WikiController extends Controller
@@ -9,6 +9,19 @@ class WikiController extends Controller
     public function __construct()
     {
         $this->middleware('editor');
+    }
+
+    /**
+     * 
+     * Render add wiki page
+     */
+    public function showAddWiki()
+    {
+        if(Auth::check()){
+            $message = Message::with('user')->groupBy('user_id')->where('receiver_id', auth()->user()->id)->where('read', false)->get();
+            return view('wiki.additemwiki', compact('message'));
+        }
+        return view('wiki.additemwiki');
     }
 }
 
