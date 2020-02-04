6617d2a7bf84398fa2100f42192dd784965b4704
polowis <tranphuochung0912@gmail.com>
2020-02-04 11:57:28 +1100

    refactor: use PriceTraits functions instead of current functions

--- app/Http/Controllers/ItemsController.php
+++ app/Http/Controllers/ItemsController.php
@@ -2,14 +2,16 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\StoreIrunaItem;
 use App\Items;
 use Illuminate\Http\Request;
 use Auth;
 use RealRashid\SweetAlert\Facades\Alert;
+use App\Traits\PriceTrait;
+
 class ItemsController extends Controller
 {
-    //
+    use PriceTrait;
+    
     public function update($id){
         if(Auth::check()){
             $item = Items::where('item_id', $id)->firstOrFail();
@@ -17,44 +19,9 @@ class ItemsController extends Controller
                 abort(404);
             }
             else{
-                if(strlen(request('price')) > 12){
-                    $item->price = 999999999999;
-                }
-                else if(strlen(request('price')) < 1){
-                    $item->price = 1;
-                } else{
-                    if(in_array(strtolower(substr(request('price'), -1)), StoreIrunaItem::PriceType)){
-                        $priceDenote = strtolower(substr(request('price'), -1));
-                        $priceNumber = substr(request('price'), 0, -1);
-                        try{
-                            if($this->validConvertPrice($priceDenote, $priceNumber)){
-                                $price = $this->convertPrice($priceDenote, $priceNumber);
-                                $item->price = $price;
-                            } 
-                        } catch(Exception $e){
-            
-                        }
-                    }
-                    if(is_numeric(request('price'))){
-                        $item->price = request('price');
-                    } 
-                }
-                
-                
-                if($this->validNumber(request('quantity'))){
-                    if((int)request('quantity') > 9999){
-                        $item->quantity = 9999;
-                    } 
-                    else if((int)request('quantity') < 1)
-                    {
-                        $item->quantity = 1;
-                    } else{
-                        $item->quantity = request('quantity');
-                    }
-                    
-                } 
+                $item->price = $this->updatePrice(request('price'));
+                $item->quantity = $this->updateQuantity(request('quantity'));
                 $item->save();
-
                 Alert::toast('Successfully edited an item', 'success');
                 return redirect()->back();
             }
@@ -76,94 +43,7 @@ class ItemsController extends Controller
         }
     }
 
-    public function validNumber($number){
-        $number = (string) $number;
-        if(ctype_digit($number)){
-            return true;
-        }
-        else{
-            return false;
-        }
-        
-    }
-
-    public function validConvertPrice($priceDenote, $priceNumber){
-        if($priceDenote == 'b'){
-            
-            if($this->validBillionPrice($priceDenote, $priceNumber)){
-                return true;
-            }else{
-                return false;
-            }
-            
-        }
-        if($priceDenote == 'm'){
-            
-            if($this->validMillionPrice($priceDenote, $priceNumber)){
-                return true;
-            } else{
-                return false;
-            }
-        }
-
-        if($priceDenote == 'k'){
-            if($this->validThousandsPrice($priceDenote, $priceNumber)){
-                return true;
-            } else{
-                return false;
-            }
-        }
-    }
-
-    private function validBillionPrice($priceDenote, $priceNumber){
-        if(is_numeric($priceNumber)){
-                
-            if((int)$priceNumber * 1000000000 > 999999999999){
-                return false;
-            } else{
-                
-                return true;
-            }
-        } else{
-            return false;
-        }
-    }
-
-    private function validMillionPrice($priceDenote, $priceNumber){
-        if(is_numeric($priceNumber)){
-            if((int)$priceNumber * 1000000 > 999999999999){
-                return false;
-            } else{
-                return true;
-            }
-        } else{
-            return false;
-        }
-    }
-
-    private function validThousandsPrice($priceDenote, $priceNumber){
-        if(is_numeric($priceNumber)){
-            if((int)$priceNumber * 1000 > 999999999999){
-                return false;
-            } else{
-                return true;
-            }
-        } else{
-            return false;
-        }
-    }
-
-    public function convertPrice($priceDenote, $priceNumber){
-        if($priceDenote == 'b'){
-            return (int)$priceNumber * 1000000000;
-        } 
-        if($priceDenote == 'm'){
-           return (int)$priceNumber * 1000000;
-        }
-        if($priceDenote == 'k'){
-            return (int)$priceNumber * 1000;
-        }
-    }
+    
 
 
 }
