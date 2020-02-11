5fcf847a26538114417e0a85424f027c22430632
polowis <tranphuochung0912@gmail.com>
2020-02-11 19:23:12 +1100

    fix: isAdmin should be set to 0 as default

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -22,8 +22,9 @@ $factory->define(User::class, function (Faker $faker) {
         'email' => $faker->unique()->safeEmail,
         'email_verified_at' => now(),
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', 
-        'isAdmin' => '1',
+        'isAdmin' => '0',
         'activate' => '1',
+        'editor' => '0',
         'remember_token' => Str::random(10),
     ];
 });
