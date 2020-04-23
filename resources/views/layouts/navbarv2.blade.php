238e059e5593305bbfc6da80c95c5723f71ff2b7
17kibr <17kibr@stockholmscience.se>
2020-04-23 19:26:19 +0200

    reverted

--- resources/views/layouts/navbarv2.blade.php
+++ resources/views/layouts/navbarv2.blade.php
@@ -21,6 +21,20 @@
 <section class="elementor-element elementor-element-7be46fa elementor-section-full_width elementor-hidden-phone elementor-section-height-default elementor-section-height-default elementor-section elementor-top-section" data-id="7be46fa" data-element_type="section" data-settings="{&quot;background_background&quot;:&quot;classic&quot;}">
     <div class="elementor-container elementor-column-gap-extended">
         <div class="elementor-row">
+            <div class="elementor-element elementor-element-f75e0ad elementor-column elementor-col-25 elementor-top-column" data-id="f75e0ad" data-element_type="column">
+                <div class="elementor-column-wrap  elementor-element-populated">
+                    <div class="elementor-widget-wrap">
+                        <div class="elementor-element elementor-element-3aa165e elementor-widget elementor-widget-image" data-id="3aa165e" data-element_type="widget" data-widget_type="image.default">
+                            <div class="elementor-widget-container">
+                                <div class="elementor-image">
+                                    <a href="https://irunastall.com/">
+                                        <img width="2123" height="1000" src="../wp-content/uploads/2020/04/irunastalllogo.png" class="attachment-full size-full wp-post-image" alt="" srcset="http://137.74.152.181/wp-content/uploads/2020/04/irunastalllogo.png 2123w, http://137.74.152.181/wp-content/uploads/2020/04/irunastalllogo-300x141.png 300w, http://137.74.152.181/wp-content/uploads/2020/04/irunastalllogo-1024x482.png 1024w, http://137.74.152.181/wp-content/uploads/2020/04/irunastalllogo-768x362.png 768w, http://137.74.152.181/wp-content/uploads/2020/04/irunastalllogo-1536x724.png 1536w, http://137.74.152.181/wp-content/uploads/2020/04/irunastalllogo-2048x965.png 2048w, http://137.74.152.181/wp-content/uploads/2020/04/irunastalllogo-416x196.png 416w, http://137.74.152.181/wp-content/uploads/2020/04/irunastalllogo-64x30.png 64w" sizes="(max-width: 2123px) 100vw, 2123px" /> </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
             <div class="elementor-element elementor-element-7b5d9df elementor-column elementor-col-25 elementor-top-column" data-id="7b5d9df" data-element_type="column">
                 <div class="elementor-column-wrap">
                     <div class="elementor-widget-wrap">
