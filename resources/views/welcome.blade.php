a2e71df02f36fbe265a33e2d3485825a95d8470f
17kibr <17kibr@stockholmscience.se>
2020-05-25 12:59:38 +0200

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -100,7 +100,7 @@
                                                 <form action="/search" name="form1" method="GET">
                                                     @csrf
                                                 <div class="search-bar">
-                                                    <div class="icon" style="background-image:url(https://new.irunastall.com/img/searchicon.png);"></div>
+                                                    <div class="icon" style="background-image:url({{ asset('img/searchicon.png') }});"></div>
                                                     
                                                     <input id="search" name="search" type="text" placeholder="Search for any equipment, xtal, al, material or relic">
                                                 </div>
