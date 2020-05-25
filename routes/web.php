ab78ea9c06fa79ba4316c8839d45729a6d7ffc1f
17kibr <17kibr@stockholmscience.se>
2020-05-25 18:29:10 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,7 @@ Route::get('/', 'HomeController@index');
 Auth::routes(['verify' => true]);
 
 Route::get('/about', 'HomeController@about');
+Route::get('/updates', 'HomeController@updates');
 
 Route::get('/account', 'HomeController@account');
 
@@ -24,9 +25,6 @@ Route::get('/account', 'HomeController@account');
 
 Route::get('/scammers', function () {
     return view('scammers');
-});
-Route::get('/updates', function () {
-    return view('updates');
 });
 
 Route::get('/tutorial', 'HomeController@tutorial');
