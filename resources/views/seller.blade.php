35af9e024ff5250c67552fa16b33e494a61ee0ba
17kibr <17kibr@stockholmscience.se>
2020-06-06 18:36:28 +0200

    Update seller.blade.php

--- resources/views/seller.blade.php
+++ resources/views/seller.blade.php
@@ -63,188 +63,189 @@
             </div>
          </div>
          <div class="container">
-           <div class="row">
-             <div class="col"></div>
-             <div class="col-12">
-               @if($equipSearch->count() >= 1)
-         <table>
-            <h2>Equipment</h2>
-            <br>
-            <thead>
-               <tr>
-                  <th scope="col">Name</th>
-                  <th scope="col">ATK</th>
-                  <th scope="col">DEF</th>
-                  <th scope="col">Refinement</th>
-                  <th scope="col">Slots</th>
-                  <th scope="col">Slot1</th>
-                  <th scope="col">Slot2</th>
-                  <th scope="col">Ability</th>
-                  <th scope="col">Ability Lv</th>
-                  <th scope="col">Price</th>
-               </tr>
-            </thead>
-            <tbody>
-               @foreach ($equipSearch as $equip)
-               <tr>
-                  <td data-label="Name"><a href="item/equip/{{$equip->item_id}}">{{ $equip->name }}</a></td>
-                  <td data-label="ATK">{{ $equip->atk }}</td>
-                  <td data-label="DEF">{{ $equip->def }}</td>
-                  <td data-label="Refinement">{{ $equip->refinement }}</td>
-                  <td data-label="Slots">{{ $equip->slots }}</td>
-                  @if ($equip->slots == 0)
-                  <td data-label="Slot1">/</td>
-                  <td data-label="Slot2">/</td>
-                  @elseif ($equip->slots == 1)
-                  @if ($equip->slot1 == null)
-                  <td data-label="Slot1">empty</td>
-                  <td data-label="Slot2">/</td>
-                  @else
-                  <td data-label="Slot1">{{ $equip->slot1 }}</td>
-                  <td data-label="Slot2">/</td>
+            <div class="row">
+               <div class="col"></div>
+               <div class="col-12">
+                  <br>
+                  @if($equipSearch->count() >= 1)
+                  <table>
+                     <h2 style="color:white;">Equipment</h2>
+                     <br>
+                     <thead>
+                        <tr>
+                           <th scope="col">Name</th>
+                           <th scope="col">ATK</th>
+                           <th scope="col">DEF</th>
+                           <th scope="col">Refinement</th>
+                           <th scope="col">Slots</th>
+                           <th scope="col">Slot1</th>
+                           <th scope="col">Slot2</th>
+                           <th scope="col">Ability</th>
+                           <th scope="col">Ability Lv</th>
+                           <th scope="col">Price</th>
+                        </tr>
+                     </thead>
+                     <tbody>
+                        @foreach ($equipSearch as $equip)
+                        <tr>
+                           <td data-label="Name"><a href="item/equip/{{$equip->item_id}}">{{ $equip->name }}</a></td>
+                           <td data-label="ATK">{{ $equip->atk }}</td>
+                           <td data-label="DEF">{{ $equip->def }}</td>
+                           <td data-label="Refinement">{{ $equip->refinement }}</td>
+                           <td data-label="Slots">{{ $equip->slots }}</td>
+                           @if ($equip->slots == 0)
+                           <td data-label="Slot1">/</td>
+                           <td data-label="Slot2">/</td>
+                           @elseif ($equip->slots == 1)
+                           @if ($equip->slot1 == null)
+                           <td data-label="Slot1">empty</td>
+                           <td data-label="Slot2">/</td>
+                           @else
+                           <td data-label="Slot1">{{ $equip->slot1 }}</td>
+                           <td data-label="Slot2">/</td>
+                           @endif
+                           @elseif ($equip->slots == 2)
+                           @if ($equip->slot1 != null)
+                           @if ($equip->slot2 != null)
+                           <td data-label="Slot1">{{ $equip->slot1 }}</td>
+                           <td data-label="Slot2">{{ $equip->slot2 }}</td>
+                           @else
+                           <td data-label="Slot1">{{ $equip->slot1 }}</td>
+                           <td data-label="Slot2">empty</td>
+                           @endif
+                           @elseif ($equip->slot1 == null)
+                           @if ($equip->slot2 == null)
+                           <td data-label="Slot1">empty</td>
+                           <td data-label="Slot2">empty</td>
+                           @else
+                           <td data-label="Slot1">empty</td>
+                           <td data-label="Slot2">{{ $equip->slot2 }}</td>
+                           @endif
+                           @endif
+                           @endif
+                           @if ($equip->ability == null)
+                           <td data-label="Ability">none</td>
+                           @else
+                           <td data-label="Ability">{{ $equip->ability }}</td>
+                           @endif
+                           <td data-label="Ability Lv">{{ $equip->ability_level}}</td>
+                           <td data-label="Price">{{ number_format($equip->price )}}</td>
+                        </tr>
+                        @endforeach
+                     </tbody>
+                  </table>
+                  {{$equipSearch->appends(['search' => request('search'), 'xtalPage' => request('xtalPage'), 'equipPage' => request('equipPage'), 'itemPage' => request('itemPage'), 'relicPage' => request('relicPage'), 'alPage' => request('alPage')])->fragment('equip')->links()}}
                   @endif
-                  @elseif ($equip->slots == 2)
-                  @if ($equip->slot1 != null)
-                  @if ($equip->slot2 != null)
-                  <td data-label="Slot1">{{ $equip->slot1 }}</td>
-                  <td data-label="Slot2">{{ $equip->slot2 }}</td>
-                  @else
-                  <td data-label="Slot1">{{ $equip->slot1 }}</td>
-                  <td data-label="Slot2">empty</td>
+                  @if($itemSearch->count() >= 1)
+                  <table>
+                     <br>
+                     <h2 style="color:white;">Materials</h2>
+                     <br>
+                     <thead>
+                        <tr>
+                           <th scope="col">Name</th>
+                           <th scope="col">QTY</th>
+                           <th scope="col">Price</th>
+                        </tr>
+                     </thead>
+                     <tbody>
+                        @foreach($itemSearch as $item)
+                        <tr>
+                           <td data-label="Name">
+                              <a href="/item/material/{{$item->item_id}}">
+                                 {{ $item->name }}
+                           </td>
+                           <td data-label="QTY">{{ $item->quantity }}</td>
+                           <td data-label="Price">{{ number_format($item->price) }}</td>
+                        </tr>
+                        @endforeach
+                     </tbody>
+                  </table>
+                  {{$itemSearch->appends(['search' => request('search'), 'xtalPage' => request('xtalPage'), 'equipPage' => request('equipPage'), 'itemPage' => request('itemPage'), 'relicPage' => request('relicPage'), 'alPage' => request('alPage')])->fragment('material')->links() }}
                   @endif
-                  @elseif ($equip->slot1 == null)
-                  @if ($equip->slot2 == null)
-                  <td data-label="Slot1">empty</td>
-                  <td data-label="Slot2">empty</td>
-                  @else
-                  <td data-label="Slot1">empty</td>
-                  <td data-label="Slot2">{{ $equip->slot2 }}</td>
-                  @endif
-                  @endif
-                  @endif
-                  @if ($equip->ability == null)
-                  <td data-label="Ability">none</td>
-                  @else
-                  <td data-label="Ability">{{ $equip->ability }}</td>
-                  @endif
-                  <td data-label="Ability Lv">{{ $equip->ability_level}}</td>
-                  <td data-label="Price">{{ number_format($equip->price )}}</td>
-               </tr>
-               @endforeach
-            </tbody>
-         </table>
-         {{$equipSearch->appends(['search' => request('search'), 'xtalPage' => request('xtalPage'), 'equipPage' => request('equipPage'), 'itemPage' => request('itemPage'), 'relicPage' => request('relicPage'), 'alPage' => request('alPage')])->fragment('equip')->links()}}
-         @endif
-         @if($itemSearch->count() >= 1)
-         <table>
-            <br>
-            <h2>Materials</h2>
-            <br>
-            <thead>
-               <tr>
-                  <th scope="col">Name</th>
-                  <th scope="col">QTY</th>
-                  <th scope="col">Price</th>
-               </tr>
-            </thead>
-            <tbody>
-               @foreach($itemSearch as $item)
-               <tr>
-                  <td data-label="Name">
-                     <a href="/item/material/{{$item->item_id}}">
-                        {{ $item->name }}
-                  </td>
-                  <td data-label="QTY">{{ $item->quantity }}</td>
-                  <td data-label="Price">{{ number_format($item->price) }}</td>
-               </tr>
-               @endforeach
-            </tbody>
-         </table>
-         {{$itemSearch->appends(['search' => request('search'), 'xtalPage' => request('xtalPage'), 'equipPage' => request('equipPage'), 'itemPage' => request('itemPage'), 'relicPage' => request('relicPage'), 'alPage' => request('alPage')])->fragment('material')->links() }}
-         @endif
-         @if($xtalSearch->count() >= 1)
-         <table>
-         <br>
-         <h2>Xtals</h2>
-         <br>
-         <thead>
-         <tr>
-         <th scope="col">Name</th>
-         <th scope="col">QTY</th>
-         <th scope="col">Price</th>
-         </tr>
-         </thead>
-         <tbody>
-         @foreach ($xtalSearch as $xtal)
-         <tr>
-         <td data-label="Name"><a href="/item/xtal/{{$xtal->item_id}}">{{ $xtal->name }}</a></td>
-         <td data-label="QTY">{{ $xtal->quantity }}</td>
-         <td data-label="Price">{{ number_format($xtal->price) }}</td>
-         </tr>
-         @endforeach
-         </tbody>
-         </table>
-         {{$xtalSearch->appends(['search' => request('search'), 'xtalPage' => request('xtalPage'), 'equipPage' => request('equipPage'), 'itemPage' => request('itemPage'), 'relicPage' => request('relicPage'), 'alPage' => request('alPage')])->fragment('xtal')->links()}}
-         @endif
-         @if($alSearch->count() >= 1)
-         <table>
-            <br>
-            <h2>AL's</h2>
-            <br>
-            <thead>
-               <tr>
-                  <th scope="col">Name</th>
-                  <th scope="col">Color</th>
-                  <th scope="col">QTY</th>
-                  <th scope="col">Price</th>
-               </tr>
-            </thead>
-            <tbody>
-               @foreach($alSearch as $al)
-               <tr>
-                  @if($al->color == "Red")
-                  <td data-label="Name" style="color:red;"><a style="color:red;" href="/item/alcrystas/{{$al->item_id}}">{{ $al->name }}</a></td>
-                  @elseif($al->color == "Blue")
-                  <td data-label="Name" style="color:blue;"><a style="color:blue;" href="/item/alcrystas/{{$al->item_id}}">{{ $al->name }}</a></td>
-                  @elseif($al->color == "Green")
-                  <td data-label="Name" style="color:green;"><a style="color:green;" href="/item/alcrystas/{{$al->item_id}}">{{ $al->name }}</a></td>
-                  @endif
-                  <td data-label="Color">{{ $al->color }}</td>
-                  <td data-label="QTY">{{ $al->quantity }}</td>
-                  <td data-label="Price">{{ number_format($al->price) }}</td>
-               </tr>
-               @endforeach
-            </tbody>
-         </table>
-         {{$alSearch->appends(['search' => request('search'), 'xtalPage' => request('xtalPage'), 'equipPage' => request('equipPage'), 'itemPage' => request('itemPage'), 'relicPage' => request('relicPage'), 'alPage' => request('alPage')])->fragment('alcrystas')->links()}}
-         @endif
-         @if($relicSearch->count() >= 1)
-         <table>
-            <br>
-            <h2>Relics</h2>
-            <br>
-            <thead>
-               <tr>
+                  @if($xtalSearch->count() >= 1)
+                  <table>
+                  <br>
+                  <h2 style="color:white;">Xtals</h2>
+                  <br>
+                  <thead>
+                  <tr>
                   <th scope="col">Name</th>
                   <th scope="col">QTY</th>
                   <th scope="col">Price</th>
-               </tr>
-            </thead>
-            <tbody>
-               @foreach($relicSearch as $relic)
-               <tr>
-                  <td data-label="Name"><a href="/item/relic/{{$relic->item_id}}">{{ $relic->name }}</a></td>
-                  <td data-label="QTY">{{ $relic->quantity }}</td>
-                  <td data-label="Price">{{ number_format($relic->price) }}</td>
-               </tr>
-               @endforeach
-            </tbody>
-         </table>
-         {{$relicSearch->appends(['search' => request('search'), 'xtalPage' => request('xtalPage'), 'equipPage' => request('equipPage'), 'itemPage' => request('itemPage'), 'relicPage' => request('relicPage'), 'alPage' => request('alPage')])->fragment('reliccrystas')->links()}}
-         @endif
-             </div>
-             <div class="col"></div>
-           </div>
+                  </tr>
+                  </thead>
+                  <tbody>
+                  @foreach ($xtalSearch as $xtal)
+                  <tr>
+                  <td data-label="Name"><a href="/item/xtal/{{$xtal->item_id}}">{{ $xtal->name }}</a></td>
+                  <td data-label="QTY">{{ $xtal->quantity }}</td>
+                  <td data-label="Price">{{ number_format($xtal->price) }}</td>
+                  </tr>
+                  @endforeach
+                  </tbody>
+                  </table>
+                  {{$xtalSearch->appends(['search' => request('search'), 'xtalPage' => request('xtalPage'), 'equipPage' => request('equipPage'), 'itemPage' => request('itemPage'), 'relicPage' => request('relicPage'), 'alPage' => request('alPage')])->fragment('xtal')->links()}}
+                  @endif
+                  @if($alSearch->count() >= 1)
+                  <table>
+                     <br>
+                     <h2 style="color:white;">AL's</h2>
+                     <br>
+                     <thead>
+                        <tr>
+                           <th scope="col">Name</th>
+                           <th scope="col">Color</th>
+                           <th scope="col">QTY</th>
+                           <th scope="col">Price</th>
+                        </tr>
+                     </thead>
+                     <tbody>
+                        @foreach($alSearch as $al)
+                        <tr>
+                           @if($al->color == "Red")
+                           <td data-label="Name" style="color:red;"><a style="color:red;" href="/item/alcrystas/{{$al->item_id}}">{{ $al->name }}</a></td>
+                           @elseif($al->color == "Blue")
+                           <td data-label="Name" style="color:blue;"><a style="color:blue;" href="/item/alcrystas/{{$al->item_id}}">{{ $al->name }}</a></td>
+                           @elseif($al->color == "Green")
+                           <td data-label="Name" style="color:green;"><a style="color:green;" href="/item/alcrystas/{{$al->item_id}}">{{ $al->name }}</a></td>
+                           @endif
+                           <td data-label="Color">{{ $al->color }}</td>
+                           <td data-label="QTY">{{ $al->quantity }}</td>
+                           <td data-label="Price">{{ number_format($al->price) }}</td>
+                        </tr>
+                        @endforeach
+                     </tbody>
+                  </table>
+                  {{$alSearch->appends(['search' => request('search'), 'xtalPage' => request('xtalPage'), 'equipPage' => request('equipPage'), 'itemPage' => request('itemPage'), 'relicPage' => request('relicPage'), 'alPage' => request('alPage')])->fragment('alcrystas')->links()}}
+                  @endif
+                  @if($relicSearch->count() >= 1)
+                  <table>
+                     <br>
+                     <h2 style="color:white;">Relics</h2>
+                     <br>
+                     <thead>
+                        <tr>
+                           <th scope="col">Name</th>
+                           <th scope="col">QTY</th>
+                           <th scope="col">Price</th>
+                        </tr>
+                     </thead>
+                     <tbody>
+                        @foreach($relicSearch as $relic)
+                        <tr>
+                           <td data-label="Name"><a href="/item/relic/{{$relic->item_id}}">{{ $relic->name }}</a></td>
+                           <td data-label="QTY">{{ $relic->quantity }}</td>
+                           <td data-label="Price">{{ number_format($relic->price) }}</td>
+                        </tr>
+                        @endforeach
+                     </tbody>
+                  </table>
+                  {{$relicSearch->appends(['search' => request('search'), 'xtalPage' => request('xtalPage'), 'equipPage' => request('equipPage'), 'itemPage' => request('itemPage'), 'relicPage' => request('relicPage'), 'alPage' => request('alPage')])->fragment('reliccrystas')->links()}}
+                  @endif
+               </div>
+               <div class="col"></div>
+            </div>
          </div>
       </section>
       @include('layouts/NewIrunaStall.footer')
