dce768eefdfd224e644d3def096080d916b74f2e
17kibr <17kibr@stockholmscience.se>
2020-06-06 18:35:06 +0200

    Update seller.blade.php

--- resources/views/seller.blade.php
+++ resources/views/seller.blade.php
@@ -25,7 +25,7 @@
       <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
       <section class="mainsection">
          @include('layouts/NewIrunaStall.navbar')
-         <div class="row topinfo">
+         <div class="row topinfo topsection">
             <div class="col title">Seller's Profile</div>
             <div class="col title">Contact</div>
          </div>
