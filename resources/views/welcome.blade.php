eb3a0a0da4d72068a707d4d1d90528b35e0ebd4a
17kibr <17kibr@stockholmscience.se>
2020-06-06 15:12:50 +0200

    fixed typo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -21,7 +21,7 @@
       <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
       <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
       <section class="mainsection">
-      @include('layouts/newIrunaStall.navbar')
+      @include('layouts/NewIrunaStall.navbar')
       <div class="centraltext">
          <span>Hi! What are you looking for today?</span>
       </div>
