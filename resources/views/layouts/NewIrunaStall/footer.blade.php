154475a25e33ba0b18e7edf78d8bc37f4f871ec8
17kibr <17kibr@stockholmscience.se>
2020-06-06 15:21:50 +0200

    updated footer links

--- resources/views/layouts/NewIrunaStall/footer.blade.php
+++ resources/views/layouts/NewIrunaStall/footer.blade.php
@@ -3,9 +3,9 @@
       <div class="footer1">
          <h1>Information</h1>
          <h4><a href="/updates">Update log</a></h4>
-         <h4><a href="/updates">Upcomming features</a></h4>
-         <h4><a href="/updates">Price check(comming soon)</a></h4>
-         <h4><a href="/updates">Wiki(comming soon)</a></h4>
+         <h4><a href="/upcomming">Upcomming features</a></h4>
+         <h4><a href="/#">Price check(comming soon)</a></h4>
+         <h4><a href="/#">Wiki(comming soon)</a></h4>
       </div>
       <div class="footer2">
          <h1>Additional Links</h1>
@@ -16,8 +16,8 @@
       </div>
       <div class="footer3" style="vertical-align: top;">
          <h1>Support us</h1>
-         <h4><a href="/updates" style="color:#ff5300;">Donate</a></h4>
-         <h4><a href="/updates">Contribute to the project</a></h4>
+         <h4><a href="https://paypal.me/kianbrosebrito" style="color:#ff5300;">Donate</a></h4>
+         <h4><a href="https://github.com/KianBrose/Iruna-Stall">Contribute to the project</a></h4>
       </div>
    </div>
 </section>
