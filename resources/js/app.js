00322fa12d974e7774e89895bab527356bd10bc2
Polowis <39208974+polowis@users.noreply.github.com>
2020-06-07 18:48:33 +1000

    fix: comment out unused component

--- resources/js/app.js
+++ resources/js/app.js
@@ -19,7 +19,7 @@ window.Vue = require('vue');
 // const files = require.context('./', true, /\.vue$/i)
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+//Vue.component('example-component', require('./components/ExampleComponent.vue').default);
 Vue.component('PrivateChat', require('./components/PrivateChat.vue').default);
 
 import Vuetify from 'vuetify'
