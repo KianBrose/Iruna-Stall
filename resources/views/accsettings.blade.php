1ed531cb7426c0a3682e5a21e631ab67cd7a6db8
17kibr <17kibr@stockholmscience.se>
2020-06-10 15:09:17 +0200

    added delete account form

--- resources/views/accsettings.blade.php
+++ resources/views/accsettings.blade.php
@@ -71,7 +71,7 @@
                         <button type="submit" class="btn btn-primary mb-2">Save</button>
                       </div>
                     </div>
-                    <p><a style="color:white; " href="https://support.discord.com/hc/en-us/articles/206346498-Where-can-I-find-my-User-Server-Message-ID-">How to get Discord ID?</a></p>
+                    <p><a style="color:white;" href="https://support.discord.com/hc/en-us/articles/206346498-Where-can-I-find-my-User-Server-Message-ID-">How to get Discord ID?</a></p>
                   </form>
                   <form>
                     <div class="form-row align-items-center">
@@ -106,7 +106,7 @@
                     </div>
                   </form>
                   <hr>
-                  <h2>Change password</h2>
+                  <h2 style="color:white;">Change password</h2>
                   <form>
                     <div class="form-group row">
                       <label for="inputEmail3" class="col-sm-2 col-form-label">Email</label>
@@ -123,6 +123,27 @@
                     <div class="form-group row">
                       <div class="col-sm-10">
                         <button type="submit" class="btn btn-primary">Reset password</button>
+                      </div>
+                    </div>
+                  </form>
+                  <hr>
+                  <h2 style="color:red;">Delete account & all posts</h2>
+                  <form>
+                    <div class="form-group row">
+                      <label for="inputEmail3" class="col-sm-2 col-form-label">Email</label>
+                      <div class="col-sm-10">
+                        <input type="email" class="form-control" id="inputEmail3" placeholder="Email">
+                      </div>
+                    </div>
+                    <div class="form-group row">
+                      <label for="inputPassword3" class="col-sm-2 col-form-label">Password</label>
+                      <div class="col-sm-10">
+                        <input type="password" class="form-control" id="inputPassword3" placeholder="Password">
+                      </div>
+                    </div>
+                    <div class="form-group row">
+                      <div class="col-sm-10">
+                        <button type="submit" class="btn btn-primary">Delete account</button>
                       </div>
                     </div>
                   </form>
