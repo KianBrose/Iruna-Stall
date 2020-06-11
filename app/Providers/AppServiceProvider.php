c714bb9ee8fe739f1e3d2be621ed1a31999a1011
Abhishek Kumar <mr.kumar.abhishek@email.com>
2020-06-11 17:35:14 +0530

    bug fix for specified key was too long

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -25,6 +26,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        
+        Schema::defaultStringLength(191);
     }
 }
