5530e7a4b3cbe82d525081cba840edcdf72f980b
17kibr <17kibr@stockholmscience.se>
2020-06-11 15:32:37 +0200

    Revert "bug fix for specified key was too long"
    This reverts commit c714bb9ee8fe739f1e3d2be621ed1a31999a1011.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,7 +5,6 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\Facades\URL;
-use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -26,6 +25,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        Schema::defaultStringLength(191);
+        
     }
 }
