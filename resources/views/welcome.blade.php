0dc4744761a119ab0dcc350d1729c547a8867ca2
Abhishek Kumar <mr.kumar.abhishek@email.com>
2020-06-16 09:33:10 +0530

    include test pwa assets in welcome.blade

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -41,6 +41,8 @@
       <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
       <!-- FontAwesome -->
       <script src="https://use.fontawesome.com/69ee5d596f.js"></script>
+      <link rel="manifest" href="manifest.json">
+      <script src="js/serviceLoader.js" type="text/javascript"></script>
    </head>
    <body>
       <!-- Bootstrap js -->
